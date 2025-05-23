954006a94b7f0891ec1d8ee4cf014ede54dbe432
Victor <victorsouzalima2064@gmail.com>
2025-05-23 13:19:57 -0300

    class Neuron implemented

---
+++ Neuron/neuron.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Neuron:
+
+    def __init__(self, weights, learningRate = 0.01):
+        self.weights = weights
+        self.bias = 1
+        self.biasWeight = 0.1
+        self.learningRate = learningRate
+
+    def output(self, inputs):
+        return np.dot(self.weights, inputs) + self.bias*self.biasWeight
+    
+    def activation(self, output):
+        return 0 if output < 0 else 1
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from Perceptron.perceptron import perceptron
+from Neuron.neuron import Neuron
 
 inputs = [1, 0]
 weights = [0.5, 0.2]
 
-perceptron(inputs, weights)
+n = Neuron(weights)
+
+output = n.output(inputs)
+
+print(n.activation(output))
