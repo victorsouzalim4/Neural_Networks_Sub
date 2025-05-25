1153207404302a6ffa199993ab4cc93bf773a908
Victor <victorsouzalima2064@gmail.com>
2025-05-25 10:17:44 -0300

    modularization applied

--- Back_Propagation/back_propagation.py
+++ Back_Propagation/back_propagation.py
@@ -1,42 +1,21 @@
 from Neuron.neuron import Neuron
+from Utils.neural_network_gen import neuralNetworkGen
 import math
 
 def backPropagation(initialLayerWidth, depth, inputs, expectedOutputs, max_epochs=50, gif_name="perceptron_training.gif"):
-    intermediaryLayersWidth = math.ceil((initialLayerWidth + 1) / 2)
 
-    layers = [] #list of neuron lists
-    layer = []  #neuron list
+    layers = neuralNetworkGen(initialLayerWidth, depth, inputs)
 
-
-    #input layer
-    for i in range(initialLayerWidth):
-        layer.append(Neuron(len(inputs[0])))
-
-    lastLayerLenght = len(layer)
-    layers.append(layer)
     
-    # hidden layers
-    for i in range(depth - 2):
-        layer = []
 
-        for j in range(intermediaryLayersWidth):
-            layer.append(Neuron(lastLayerLenght))
 
-        lastLayerLenght = len(layer)
-        layers.append(layer)
-    
-    #output layer
-    print(lastLayerLenght)
-    outputLayer = [Neuron(lastLayerLenght)]
-    layers.append(outputLayer)
+    # for i, layer in enumerate(layers):
+    #     print(f"\n🧠 Camada {i} ({len(layer)} neurônios)")
+    #     print("-" * 40)
 
-    for i, layer in enumerate(layers):
-        print(f"\n🧠 Camada {i} ({len(layer)} neurônios)")
-        print("-" * 40)
-
-        for j, neuron in enumerate(layer):
-            print(f"  🔹 Neurônio {j}: Pesos -> {neuron.weights}")
-        print("-" * 40)
+    #     for j, neuron in enumerate(layer):
+    #         print(f"  🔹 Neurônio {j}: Pesos -> {neuron.weights}")
+    #     print("-" * 40)
 
         
     
---
+++ Utils/neural_network_gen.py
@@ -0,0 +1,36 @@
+from Neuron.neuron import Neuron
+import math
+
+def neuralNetworkGen(initialLayerWidth, depth, inputs):
+    intermediaryLayersWidth = math.ceil((initialLayerWidth + 1) / 2)
+
+    layers = [] #list of neuron lists
+    layer = []  #neuron list
+
+
+    #input layer
+    for i in range(initialLayerWidth):
+        layer.append(Neuron(len(inputs[0])))
+
+    lastLayerLenght = len(layer)
+    layers.append(layer)
+    
+    # hidden layers
+    for i in range(depth - 2):
+        layer = []
+
+        for j in range(intermediaryLayersWidth):
+            layer.append(Neuron(lastLayerLenght))
+
+        lastLayerLenght = len(layer)
+        layers.append(layer)
+    
+    #output layer
+    print(lastLayerLenght)
+    outputLayer = [Neuron(lastLayerLenght)]
+    layers.append(outputLayer)
+
+    return layers
+
+        
+    
